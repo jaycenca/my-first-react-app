214144f61bd7c23a535da559350bc2984e3a7ade
Jay C <jaycenusa@gmail.com>
2025-09-15 10:08:45 -0400

    fix: remove incorrect import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css';
-import graph from './my-module/myModule.js';
 
 function App() {
   const [count, setCount] = useState(0)
